4458afa1f79e6265125570c93280c897ccc61ae4
Flo0108 <florian.zeif1@gmail.com>
2026-01-19 23:41:35 +0100

    update

--- public/script.js
+++ public/script.js
@@ -301,16 +301,9 @@ loadImages();
 
 
 
-// -------------------- Recursion Grid Parameters --------------------
-const maxSubdivision = isMobile ? 2 : 4;
-const minDistance   = isMobile ? 30 : 20;
-const baseSize      = isMobile ? 160 : 100;
-
-console.log("Recursion grid settings:", {
-  maxSubdivision,
-  minDistance,
-  baseSize
-});
+const maxSubdivision = 4;
+const minDistance = 20; // pixels for max subdivision
+const baseSize = 100;   // initial cell size in pixels
 
 
 
